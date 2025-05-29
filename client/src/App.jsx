7017f508c01379f97a0c9180e81b54ab3fe7eed9
kaif85077 <md.20213010@mnnit.ac.in>
2025-05-29 11:17:05 +0530

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+import Pooja from './pages/Pooja'
+
 
 export default function App() {
   return (
-    <div className='text-5xl text-red-500 font-bold text-center mt-20'>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/pooja" element={<Pooja />} />
+      </Routes>
+    </Router>
   )
 }
